5a6068d161f136685c596476971a81728f6f3d0f
kian-psr <kian.poser@gmail.com>
2026-04-09 09:14:52 -0400

    clean code + create second table

--- src/04_analysis.py
+++ src/04_analysis.py
@@ -21,9 +21,12 @@ if "Date" not in df.columns or "Daily Return (%)" not in df.columns:
 #-----------------------------------------------------------------------------------------------------------------
 # 1. Summary statistics table for each sector (ticker)
 #-----------------------------------------------------------------------------------------------------------------
-summary_stats = df.groupby("ticker")["Daily Return (%)"].agg(["mean", "std", "min", "max", "count"]).reset_index()
-summary_stats = summary_stats.round(3)  # Round the statistics to 3 decimal places for better readability
-
+summary_stats = (
+    df.groupby("ticker")["Daily Return (%)"]
+    .agg(["mean", "std", "min", "max", "count"]) # Calculate mean, standard deviation, min, max and count of daily returns for each ticker
+    .round(3)  # Round the statistics to 3 decimal places for better readability
+    .reset_index() 
+)
 summary_stats = summary_stats.rename(columns={
     "mean": "Mean Daily Return (%)",
     "std": "Volatility (Std Dev)",
@@ -37,4 +40,38 @@ summary_stats.to_csv(OUTPUT_TABLE / "summary_statistics.csv", index=False)
 print(f"Summary statistics saved to {OUTPUT_TABLE / 'summary_statistics.csv'}")
 
 print("Saved summary statistics table.")
+
 #-----------------------------------------------------------------------------------------------------------------
+# 2. Cumulative return figure for each sector
+#-----------------------------------------------------------------------------------------------------------------
+# this is to calculate the cumulative return for each sector and plot it over time
+
+# turn percent into decimal for the calulation of cumulative return
+df["Daily Return (Decimal)"] = df["Daily Return (%)"] / 100
+
+# Calculate the cumulative return for each ticker. Growth if £1 invested
+
+df ["cumulative return"] = (
+    (1 + df["Daily Return (Decimal)"])
+    .groupby(df["ticker"])
+    .cumprod() -1
+)
+
+# Plot the cumulative return for each sector
+plt.figure(figsize=(12, 7))
+
+for ticker in df["ticker"].unique():
+    ticker_data = df[df["ticker"] == ticker]
+    plt.plot(ticker_data["Date"], ticker_data["cumulative return"], label=ticker)
+
+plt.title("Cumulative Returns of Sector ETFs & Benchmark (SPY)")
+plt.xlabel("Date")
+plt.ylabel("Cumulative Return")
+plt.legend()
+plt.xticks(rotation=45)
+plt.tight_layout()
+
+plt.savefig(OUTPUT_FIGURE / "cumulative_returns.png")
+plt.close()
+
+print("Saved cumulative returns figure.")
